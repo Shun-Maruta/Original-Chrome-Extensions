48da685fbc4720bc3ff0625e31627d68dcbb0645
Shun <sunabura64@gmail.com>
2025-03-12 01:16:55 +0900

    回答案自動作成機能デモ完成

--- 2nd/popup.js
+++ 2nd/popup.js
@@ -9,6 +9,37 @@ document.getElementById("copyTable").addEventListener("click", () => {
     })
 })
 
+document.getElementById("createAnswer").addEventListener("click", () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        if (tabs.length === 0) return
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            function: createAnswer
+        })
+        window.close()
+    })
+})
+
+const createAnswer = async () => {
+    const ticket_id = document.getElementById("ticket_id").textContent.trim()
+    const title = document.getElementById("title").textContent.trim()
+    const question = document.getElementById("question").textContent.trim()
+
+    console.log(ticket_id, title, question)
+
+    const response = await fetch(chrome.runtime.getURL("template.txt"))
+    const template = await response.text()
+
+    const result =  template.replace("{{ticket_id}}", ticket_id).replace("{{title}}", title).replace("{{question}}", question)
+
+    const copyToClipboard = (text) => {
+        navigator.clipboard.writeText(text)
+            .then(() => alert("テーブルがコピーされました！"))
+            .catch(err => alert("コピーに失敗しました: " + err))
+    }
+    
+    setTimeout(() => copyToClipboard(result), 100)
+}
 
 
 const copyTableData = () => {
